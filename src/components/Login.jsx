11d54953deb6f9162b22bf2d10f3c897c70cae2f
fatih-alkan <fatihalkansw@gmail.com>
2025-06-19 16:53:57 +0300

    Login.jsx güncellendi

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,54 @@
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import { useState } from 'react';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
-export default function Login(){
+import { Form, FormGroup, Label, Input, Button, FormFeedback } from 'reactstrap';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+export default function Login(){
+    const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const [errors, setErrors] = useState({ email: '', password: '' });
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    const [isActive, setIsActive] = useState(true)
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    let valid = true;
+    const newErrors = { email: '', password: '' };
+
+    if (!emailRegex.test(formData.email)) {
+      newErrors.email = 'Geçerli bir email girin.';
+      valid = false;
+    }
+
+    if (!passwordRegex.test(formData.password)) {
+      newErrors.password =
+        'Şifre en az 8 karakter olmalı, büyük harf, küçük harf, sayı ve özel karakter içermeli.';
+      valid = false;
+    }
+
+    setErrors(newErrors);
+
+    if (valid) {
+      console.log('Form gönderildi:', formData);
+    }
+  }
+  useEffect(() => {
+    const isEmailValid = emailRegex.test(formData.email);
+    const isPasswordValid = passwordRegex.test(formData.password);
+    setIsActive(!(isEmailValid && isPasswordValid));
+  }, [formData]);
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>         
       <FormGroup>
         <Label for="exampleEmail">Email</Label>
         <Input
@@ -15,6 +58,7 @@ export default function Login(){
           type="email"
           onChange={handleChange}
         />
+        <FormFeedback>{errors.email}</FormFeedback>
       </FormGroup>
       <FormGroup>
         <Label for="examplePassword">Password</Label>
@@ -25,9 +69,10 @@ export default function Login(){
           type="password"
           onChange={handleChange}
         />
+        <FormFeedback>{errors.password}</FormFeedback>
       </FormGroup>
-      <Button color="primary" type="submit">
-        Sign In
+      <Button color="primary" type="submit" disabled={isActive}>
+        Login
       </Button>
     </Form>
     )
